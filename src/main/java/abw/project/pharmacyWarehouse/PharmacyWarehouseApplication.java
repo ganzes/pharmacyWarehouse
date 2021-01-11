a060c9e4b1e11a6e94d4e56e92728247449844c3
ganzes <charczuk.arkadiusz@gmail.com>
2021-01-11 15:55:41 +0100

    initial commit, added swagger, h2 db

---
+++ src/main/java/abw/project/pharmacyWarehouse/PharmacyWarehouseApplication.java
@@ -0,0 +1,13 @@
+package abw.project.pharmacyWarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abw/project/pharmacyWarehouse/PharmacyWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package abw.project.pharmacyWarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
